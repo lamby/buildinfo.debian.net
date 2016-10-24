101abc780622217b8de4dfaf68e9bddedbc6662d
Chris Lamb <lamby@debian.org>
2016-10-24 09:45:20 +0200

    Show number of submissions
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- bidb/static/views.py
+++ bidb/static/views.py
@@ -4,7 +4,9 @@ from bidb.buildinfo.buildinfo_submissions.models import Submission
 
 def landing(request):
     latest = Submission.objects.all()[:20]
+    num_submissions = Submission.objects.count()
 
     return render(request, 'static/landing.html', {
         'latest': latest,
+        'num_submissions': num_submissions,
     })
