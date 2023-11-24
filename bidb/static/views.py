058b65b242a51f24bd2fa46348baeba763e1ac08
Chris Lamb <lamby@debian.org>
2023-11-24 13:33:20 +0000

    Reflect manual changes.

--- bidb/static/views.py
+++ bidb/static/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
 
-from bidb.buildinfo.buildinfo_submissions.models import Submission
-
 def landing(request):
-    latest = Submission.objects.select_related('key').order_by('-created')[:20]
-    num_submissions = Submission.objects.count()
-
-    return render(request, 'static/landing.html', {
-        'latest': latest,
-        'num_submissions': num_submissions,
-    })
+    return render(request, 'static/landing.html')
